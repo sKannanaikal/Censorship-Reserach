0afcb44560646d18cabba9ca9d18713c42ae0f5d
Sean Kannanaikal <stk5106@g.rit.edu>
2024-03-26 15:06:46 -0400

    Update script2.py

--- script2.py
+++ script2.py
@@ -3,9 +3,17 @@ from glob import glob
 
 blackList = set()
 countries = ['CN', 'IR', 'KP', 'TM', 'SA']
-users = 0
-infringements = 0
-trend = {}
+chinatrend = {}
+irantrend = {}
+northKoreatrend = {}
+turkmenistantrend = {}
+saudiarabiatrend = {}
+
+def updateTrendDictionary(countryDictionary, timestamp):
+    if timestamp in countryDictionary:
+        countryDictionary[timestamp] += 1
+    else:
+        countryDictionary[timestamp] = 1
 
 def main():
     global blackList
@@ -17,31 +25,34 @@ def main():
         for line in chinaCensoredSites.readlines():
             if line[0] == '.':
                 blackList.add(line[1:].strip().lower())
-                print(line[1:].strip().lower())
             else:
                 blackList.add(line.strip().lower())
-                print(line.strip().lower())
     database = glob('genesisvictims/*.json')
     for index, victim in enumerate(database):
         with open(victim) as victimFile:
             data = json.load(victimFile)
-        victimAccounts = data['domain_names']
-        victimCountry = data['country']
-        timestamp = data['timestamp'][:4]
-        for country in countries:
-            users = 0
-            infringements = 0
-            trend = {}
-            for account in victimAccounts:
-                if account.lower() in blackList and victimCountry == 'CN':
-                    if timestamp in trend:
-                        trend[timestamp] += 1
-                    else:
-                        trend[timestamp] = 1
-            print(f'Analysis complete for {country}')
-            print(trend)
-
-
-    
+        try:
+            victimAccounts = data['domain_names']
+            victimCountry = data['country']
+            timestamp = data['timestamp'][:4]
+        except:
+            continue
+        for account in victimAccounts:
+            if account.lower() in blackList and victimCountry in countries:
+                if victimCountry == 'CN':
+                    updateTrendDictionary(chinatrend, timestamp)
+                elif victimCountry == 'IR':
+                    updateTrendDictionary(irantrend, timestamp)
+                elif victimCountry == 'KP':
+                    updateTrendDictionary(northKoreatrend, timestamp)
+                elif victimCountry == 'TM':
+                    updateTrendDictionary(turkmenistantrend, timestamp)
+                elif victimCountry == 'SA':
+                    updateTrendDictionary(saudiarabiatrend, timestamp)
+    print(f'China: {chinatrend}')
+    print(f'Iran: {irantrend}')
+    print(f'North Korea: {northKoreatrend}')
+    print(f'Turkmenistan: {turkmenistantrend}')
+    print(f'Saudia Arabia: {saudiarabiatrend}')
 
 main()
