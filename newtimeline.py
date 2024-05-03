2f19a927ad0a81c9a7fba5b473f436cafc2df5fd
Sean Kannanaikal <stk5106@g.rit.edu>
2024-05-03 07:40:56 -0400

    Update newtimeline.py

--- newtimeline.py
+++ newtimeline.py
@@ -26,19 +26,18 @@ censorSA = {}
 
 def updateTrendDictionary(countryDictionary, timestamp):
     if timestamp in countryDictionary:
-        countryDictionary[timestamp] += 1
-    else:
-        countryDictionary[timestamp] = 1
+		countryDictionary[timestamp] += 1
+	else:    
+		countryDictionary[timestamp] = 1
 
 def createBlackListSet():
-	blackList = set()
 	with open('./china.csv', 'r') as chinaCensoredSites:
-        for line in chinaCensoredSites.readlines():
-            if line[0] == '.':
-                blackList.add(line[1:].strip().lower())
-            else:
-                blackList.add(line.strip().lower())
-    return blackList
+		for line in chinaCensoredSites.readlines():
+			if line[0] == '.':
+				blackList.add(line[1:].strip().lower())
+			else:
+				blackList.add(line.strip().lower())
+	return blackList
 
 def generatePercentagesTimeline(censorSet, totalSet,country):
 	print(f'*****Timeline for {country}*****')
@@ -65,45 +64,45 @@ def main():
 	global countries
 	blackList = createBlackListSet()
 	database = glob('database/*.json')
-    for index, victim in enumerate(database):
-        with open(victim) as victimFile:
-            data = json.load(victimFile)
-        try:
-            victimAccounts = data['domain_names']
-            victimCountry = data['country']
-            timestamp = data['timestamp'][:4]
-        except:
-            continue
-        for account in victimAccounts:
-        	if account.lower() in blackList and victimCountry in countries:
-        		if victimCountry == 'CN':
-                    updateTrendDictionary(censorChina, timestamp)
-                elif victimCountry == 'IR':
-                    updateTrendDictionary(censorIran, timestamp)
-                elif victimCountry == 'US':
-                    updateTrendDictionary(censorUS, timestamp)
-                elif victimCountry == 'TM':
-                    updateTrendDictionary(censorTurkmenistan, timestamp)
-                elif victimCountry == 'SA':
-                    updateTrendDictionary(censorSA, timestamp)
-           	if victimCountry in countries:
-           		if victimCountry == 'CN':
-                    updateTrendDictionary(totalChina, timestamp)
-                elif victimCountry == 'IR':
-                    updateTrendDictionary(totalIran, timestamp)
-                elif victimCountry == 'US':
-                    updateTrendDictionary(totalUS, timestamp)
-                elif victimCountry == 'TM':
-                    updateTrendDictionary(totalTurkmenistan, timestamp)
-                elif victimCountry == 'SA':
-                    updateTrendDictionary(totalSA, timestamp)
+	for index, victim in enumerate(database):
+		with open(victim) as victimFile:
+			data = json.load(victimFile)
+			try:
+				victimAccounts = data['domain_names']
+				victimCountry = data['country']
+				timestamp = data['timestamp'][:4]
+			except:
+				continue
+		for account in victimAccounts:
+			if account.lower() in blackList and victimCountry in countries:
+				if victimCountry == 'CN':
+					updateTrendDictionary(censorChina, timestamp)
+				elif victimCountry == 'IR':
+					updateTrendDictionary(censorIran, timestamp)
+				elif victimCountry == 'US':
+					updateTrendDictionary(censorUS, timestamp)
+				elif victimCountry == 'TM':
+					updateTrendDictionary(censorTurkmenistan, timestamp)
+				elif victimCountry == 'SA':
+					updateTrendDictionary(censorSA, timestamp)
+			if victimCountry in countries:
+				if victimCountry == 'CN':
+					updateTrendDictionary(totalChina, timestamp)
+				elif victimCountry == 'IR':
+					updateTrendDictionary(totalIran, timestamp)
+				elif victimCountry == 'US':
+					updateTrendDictionary(totalUS, timestamp)
+				elif victimCountry == 'TM':
+					updateTrendDictionary(totalTurkmenistan, timestamp)
+				elif victimCountry == 'SA':
+					updateTrendDictionary(totalSA, timestamp)
 
 
-        generatePercentagesTimeline(censorChina, totalChina, 'China')
-        generatePercentagesTimeline(censorIran, totalIran, 'Iran')
-        generatePercentagesTimeline(censorUS, totalUS,'USA')
-        generatePercentagesTimeline(censorTurkmenistan, totalTurkmenistan,'Turkmenistan')
-        generatePercentagesTimeline(censorSA, totalSA,'Saudi Arabia')
+	generatePercentagesTimeline(censorChina, totalChina, 'China')
+	generatePercentagesTimeline(censorIran, totalIran, 'Iran')
+	generatePercentagesTimeline(censorUS, totalUS,'USA')
+	generatePercentagesTimeline(censorTurkmenistan, totalTurkmenistan,'Turkmenistan')
+	generatePercentagesTimeline(censorSA, totalSA,'Saudi Arabia')
 
 
 if __name__ == '__main__':
